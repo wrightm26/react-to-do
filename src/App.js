49c407bd39590e702bd2207ea34d10650bf6148f
Brian <bstanton773@gmail.com>
2023-04-04 10:41:55 -0500

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 export default function App() {
     return (
-        <h1>Hello World</h1>
+        <>
+            <Navbar />
+            <div className='container'>
+                <h1>Hello World</h1>
+            </div>
+        </>
     )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar() {
+
+    return (
+        <nav className="navbar bg-body-tertiary">
+            <div className="container-fluid">
+                <a href="/" className="navbar-brand">To Do List</a>
+            </div>
+        </nav>
+    )
+}
