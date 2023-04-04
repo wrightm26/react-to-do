270102a25b3728b89bdb236bcd84cfe13c8e01f1
Brian <bstanton773@gmail.com>
2023-04-04 11:57:13 -0500

    Add Form and List

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
+import ToDoForm from './components/ToDoForm';
+import ToDoList from './components/ToDoList';
 
 export default function App() {
+
+    const [toDos, setToDos] = useState([]);
+
+    function addToList(task){
+        console.log(task);
+        setToDos([...toDos, task]);
+    };
+
+
     return (
         <>
             <Navbar />
             <div className='container'>
-                <h1>Hello World</h1>
+                <ToDoForm addToList={addToList} />
+                <ToDoList taskList={toDos} />
             </div>
         </>
     )
